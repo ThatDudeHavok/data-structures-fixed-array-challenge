18854234284b73753019945b7c17f7cd2669a13e
Nicholas J. Olson <nicholasjamesolson@gmail.com>
2016-10-06 11:14:56 -0500

    Add tests in pending state

--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,5 +1,19 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
-  
+  describe "#new" do
+    xit "should create an array of specified size"
+  end
+
+  describe "#get" do
+    xit "should get element"
+
+    xit "should throw OutOFBoundsException"
+  end
+
+  describe "#set" do
+    xit "should add element to existing array"
+
+    xit "should throw OutOFBoundsException"
+  end
 end
